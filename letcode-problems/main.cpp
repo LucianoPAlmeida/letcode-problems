0ea605b589c4e8b5f8f9eac9578dd244b534a95b
Luciano Almeida <passos.luciano@outlook.com>
2020-05-14 16:19:41 -0300

    Initial Commit

---
+++ letcode-problems/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  letcode-problems
+//
+//  Created by Luciano Almeida on 14/05/20.
+//  Copyright © 2020 Luciano Almeida. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
